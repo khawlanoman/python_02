90327c584f42df8a4ca72d6f29559396e595c16d
khawlanoman <khawlanomanot7@gmail.com>
2026-01-17 13:08:01 +0100

    .

--- ex3/ft_finally_block.py
+++ ex3/ft_finally_block.py
@@ -1,2 +1,32 @@
 def  water_plants(plant_list):
-    
+    plant_list
+    print("Opening watering system")
+    try:
+        for plant in plant_list:
+            if plant is None:
+                raise 
+            print(f"Watering {plant}")
+    except:
+         print(f"Error: Cannot water {plant} - invalid plant!")
+    print("Closing watering system (cleanup)")
+
+
+def test_watering_system():
+    plant_list={"tomato","lettuce","carrots"}
+    print("Testing normal watering...")
+   
+    try:
+        water_plants(plant_list)
+        print("Watering completed successfully!")
+    except:
+        print("Error")
+   
+    print("\nTesting normal watering...")
+    try:
+        plant_list_error={"tomato",None,"carrots"}
+        water_plants(plant_list_error)
+
+    finally:
+        print("\nCleanup always happens, even with errors!")
+
+test_watering_system()
---
+++ ex4/ft_raise_errors.py
@@ -0,0 +1,40 @@
+def check_plant_health(plant_name, water_level, sunlight_hours):
+    if (plant_name == ""):
+        print("\nTesting empty plant name...")
+        raise ValueError("Plant name cannot be empty!\n")
+    elif (water_level < 1  or water_level > 10):
+        print("\nTesting bad water level...")
+        raise ValueError(f"Water level {water_level} is too high (max 10)\n")
+    elif (sunlight_hours < 2 or sunlight_hours > 12):
+        print("\nTesting bad sunlight hours...")
+        raise ValueError(f"Sunlight hours {sunlight_hours} is too low (min 2)\n")
+    elif (plant_name != ""):
+        print("\nTesting good values...")
+        print("Plant 'tomato' is healthy!")
+    
+
+def test_plant_checks():
+    print("=== Garden Plant Health Checker ===\n")
+    try:
+        check_plant_health("tomato",1,10)
+    except ValueError as error:
+        print(f"Error: {error}")
+    
+    try:
+        check_plant_health("",1,10)
+    except ValueError as error:
+        print(f"Error: {error}")
+    
+    try:
+        check_plant_health("tomato",15,10)
+    except ValueError as error:
+        print(f"Error: {error}")
+   
+    try:
+        check_plant_health("tomato",1,0)
+    except ValueError as error:
+        print(f"Error: {error}")
+    
+    print("\nAll error raising tests completed!")
+
+test_plant_checks()
